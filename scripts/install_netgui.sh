6122531290b70179e5884c7789dcd644b0b945d4
Javier Ruiz Ramos <rura.javier@gmail.com>
2024-03-19 16:55:42 +0100

    netgui

--- scripts/install_netgui.sh
+++ scripts/install_netgui.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Check number of arguments
+if [ $# -ne 0 ]
+then
+    echo "This script does not take any arguments."
+    exit 1
+fi
+
 # Ubuntu Shell is none interactive
 eval "$(cat ~/.bashrc | grep export)"
 
@@ -20,44 +27,37 @@ fi
 # Check if 32 bit arquitecture is enabled in apt
 if ! dpkg --print-foreign-architectures | grep i386
 then
+    echo
     echo "Enabling 32 bit architecture..."
-    sudo dpkg --add-architecture i386
+    sudo dpkg --add-architecture i386 > /dev/null 2>&1
     sudo apt update
     if [ $? -ne 0 ]
     then
-        echo "Failed to enable 32 bit architecture"
+        echo "Failed to enable 32 bit architecture."
         exit 1
     fi
+    echo "32 bit architecture enabled successfully."
 fi
 
 # Check if eif repo is already installed
 if [ -f ./scripts/eif_repo_install.sh ]
 then
+    echo
     echo "Attempting to install eif repo..."
     ./scripts/eif_repo_install.sh
     if [ $? -ne 0 ]
     then
-        echo "Failed to install eif repo"
+        echo "Failed to install eif repo."
         exit 1
     fi
-fi
-
-# Check number of arguments
-if [ $# -ne 0 ]
-then
-    echo "This script does not take any arguments."
-    exit 1
-fi
-
-# Check if eif repo is installed
-if [ ! -f /etc/apt/sources.list.d/lablinuxrepo.list ]
-then
-    echo "Eif repo is not installed. Needed for netgui installation."
-    exit 0
+    echo "Eif repo installed successfully."
 fi
 
 # Update
+echo
+echo "Running apt update..."
 sudo apt update > /dev/null 2>&1
+echo "Apt update finished."
 
 # Packages to install
 packages="gnome-terminal konsole netgui"
@@ -69,16 +69,18 @@ do
     then
         echo "Package $p already installed"
     else
-        sudo apt install -y $p > /dev/null 2>&1
+        echo
+        sudo apt install -y $p
 
         # Check if package was installed succesfully
         if [ $? -ne 0 ];
         then
-            echo "Package $p failed to install"
+            echo "Package $p failed to install."
             exit 1
         fi
     fi
 done
 
+echo
 echo "Netgui installed successfully."
 echo "You can run it by typing netgui.sh in terminal."
